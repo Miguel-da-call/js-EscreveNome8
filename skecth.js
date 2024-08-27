15cce0ed73c4604e6a5caaa9d79ccf249c6b917d
Miguel-da-call <00001105152686SP@al.educacao.sp.gov.br>
2024-08-27 14:50:56 -0300

    Update skecth.js

--- skecth.js
+++ skecth.js
@@ -1,1 +1,15 @@
+function setup() {
+  createCanvas(600, 600);
+  background("black");
+}
 
+function draw() {
+ 
+stroke("brown");
+fill("white");
+  
+  // console.log(mouseIsPressed)
+  
+  if(mouseIsPressed)
+rect(mouseX, mouseY, 10, 15);
+}
